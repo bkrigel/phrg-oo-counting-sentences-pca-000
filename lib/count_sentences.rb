b2d8a46629811e9fb684cf04586dd66156318e5f
bkrigel <benjamin.krigel@gmail.com>
2018-05-21 16:42:52 -0400

    Fun Lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sent_array = self.split(/[.?!]/)
+    sent_array.reject! {|e| e == ""}
+    sent_array.count
   end
 end
